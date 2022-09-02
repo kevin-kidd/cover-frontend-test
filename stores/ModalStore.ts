9d9ff785d8b5dd846afb70b2c1468fd1639cd733
Kevin Harrison <62897443+kh-codes@users.noreply.github.com>
2022-09-02 19:28:45 -0400

    commit

--- stores/ModalStore.ts
+++ stores/ModalStore.ts
@@ -4,6 +4,14 @@ interface Modals {
     name: string
     isOpen: boolean
     title: string
+    asset?: {
+        name: {
+            wrapped: string
+            unwrapped: string
+        }
+        address: string
+    }
+    setAsset: (by: { name: { wrapped: string, unwrapped: string }, address: string}) => void
     setName: (by: string) => void
     setTitle: (by: string) => void
     setIsOpen: (by: boolean) => void
@@ -14,6 +22,7 @@ export const useModalStore = create<Modals>()(
         name: "",
         isOpen: false,
         title: "",
+        setAsset: (by: { name: { wrapped: string, unwrapped: string }, address: string}) => set(() => ({ asset: by })),
         setName: (by: string) => set(() => ({ name: by })),
         setTitle: (by: string) => set(() => ({ title: by })),
         setIsOpen: (by: boolean) => set(() => ({ isOpen: by })),
